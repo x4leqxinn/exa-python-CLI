eb19def6f3af8b42d268b7d5b2f35e5840e9e711
Jorge Quintui <jorgealequinn@gmail.com>
2023-01-02 17:28:01 -0300

    UPDATE: Main CLI

--- main.py
+++ main.py
@@ -1,13 +1,49 @@
 #!/usr/bin/venv python
 import click
+from app.database.models import MODELS, User, Task
+import settings
 
 # Create a group commands
 @click.group()
 def main(): pass
 
+# Command 
+# Args
+# Options
+# Flags -> Bool
+
 @main.command()
-def say():
-    print('Hello world!')
+def create_tables():
+    with settings.CONNECTION:
+        settings.CONNECTION.create_tables(MODELS)
+    print('--- Tablas creadas ---')
+
+@main.command()
+@click.argument('username')
+@click.option('--password','-p',prompt='Enter password',hide_input=True)
+@click.option('--email','-e',default='jorge@gmail.com')
+@click.option('--active','-a',is_flag=True,default=True)
+def create_user(username,password,email,active):
+    user = User.create(username=username,password=password,email=email,active=active)
+    if user.id:
+        print('Usuario creado exitosamente!')
+
+@main.command()
+@click.argument('title')
+@click.argument('username')
+def create_task(title,username): 
+    user = User.select().where(User.username == username).first()
+    if user:
+        task = Task.create(title=title, user=user)
+        if task.id: print('Tarea creada con exito!')
+
+@main.command()
+@click.argument('username')
+def list_tasks(username):
+    user = User.select().where(User.username == username).first()
+    if user: 
+        for task in user.tasks: print(task)
+
 
 if __name__ == '__main__':
     main()
